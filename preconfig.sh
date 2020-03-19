d425b1605091d3a4dc6ae44ecbb29b40971f887e
minseob.kim <minseob.kim@kurlycorp.com>
2020-03-19 17:10:06 +0900

    라이브 러리 버전을 최신화 하라

--- preconfig.sh
+++ preconfig.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker run --rm -u 1000:108 -v /var/log:/var/log -v "$PWD":/home/gradle/project -w /home/gradle/project \
--e SPRING_PROFILES_ACTIVE='dev' gradle:jdk11 /home/gradle/project/gradlew clean build --info
+  -e SPRING_PROFILES_ACTIVE='dev,disable-discovery' gradle:jdk11 /home/gradle/project/gradlew clean build --info
