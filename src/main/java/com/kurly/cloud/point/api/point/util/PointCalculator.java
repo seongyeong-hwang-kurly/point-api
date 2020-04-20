62bc1f0caa2c84eb6338f4abc72b19a838f1982c
minseob.kim <minseob.kim@kurlycorp.com>
2020-04-20 09:56:17 +0900

    `PointCaculator`테스트를 추가하라

--- src/main/java/com/kurly/cloud/point/api/point/util/PointCalculator.java
+++ src/main/java/com/kurly/cloud/point/api/point/util/PointCalculator.java
@@ -3,6 +3,7 @@ package com.kurly.cloud.point.api.point.util;
 public class PointCalculator {
   public static int calculateOrderPoint(int payPrice, float pointRatio) {
     if (pointRatio == 0) return 0;
-    return Math.round(payPrice * pointRatio / 100);
+    int point = Math.round(payPrice * pointRatio / 100);
+    return point == 0 ? 1 : point;
   }
 }
