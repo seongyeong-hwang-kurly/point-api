73ab55759f418e70e1635c55fcfb382c9744a8e6
minseob.kim <minseob.kim@kurlycorp.com>
2020-04-21 12:38:54 +0900

    어플리케이션명을 주입받을 수 있게 하라

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -jar -Dspring.profiles.active=$RUN_ENV -Duser.timezone=Asia/Seoul $RUN_JVM_PARAM /usr/local/bin/cloud-application.jar
+java -jar -Dspring.profiles.active=$RUN_ENV -Dspring.application.name=$APP_NAME -Duser.timezone=Asia/Seoul $RUN_JVM_PARAM /usr/local/bin/cloud-application.jar
