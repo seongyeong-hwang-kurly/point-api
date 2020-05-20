6ae781d695b0e8a97eb79acfd0a9bb5cdbef9e1d
minseob.kim <minseob.kim@kurlycorp.com>
2020-05-20 13:47:23 +0900

    공통모듈을 3.0.0 으로 업그레이드 하라

--- src/main/java/com/kurly/cloud/point/api/point/domain/consume/OrderConsumePointRequest.java
+++ src/main/java/com/kurly/cloud/point/api/point/domain/consume/OrderConsumePointRequest.java
@@ -2,13 +2,17 @@ package com.kurly.cloud.point.api.point.domain.consume;
 
 import com.kurly.cloud.point.api.point.domain.history.HistoryType;
 import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Builder
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderConsumePointRequest {
   @NotNull
   Long memberNumber;
