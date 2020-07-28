dd4867a0eb9c6ccee0c7dfd537a9007ed767d8f4
minseob.kim <minseob.kim@kurlycorp.com>
2020-07-28 17:02:27 +0900

    controller 대신 actuator를 통한 배치 실행을 가능하게 하라

---
+++ src/main/java/com/kurly/cloud/point/api/point/batch/expire/PointExpireManagementEndpoint.java
@@ -0,0 +1,48 @@
+package com.kurly.cloud.point.api.point.batch.expire;
+
+import com.kurly.cloud.api.common.util.SlackNotifier;
+import com.kurly.cloud.api.common.util.logging.FileBeatLogger;
+import com.kurly.cloud.point.api.point.batch.expire.config.PointExpireJobConfig;
+import java.time.LocalDateTime;
+import java.util.Date;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+@Slf4j
+@Component
+@ConditionalOnProperty("batch.enable")
+@RequiredArgsConstructor
+@Endpoint(id = "pointExpire")
+public class PointExpireManagementEndpoint {
+
+  private final JobLauncher jobLauncher;
+  @Qualifier("pointExpireJob")
+  private final Job pointExpireJob;
+
+  @WriteOperation
+  void executeExpireBatch(String expireTime) {
+    if (StringUtils.isEmpty(expireTime)) {
+      expireTime = LocalDateTime.now().format(PointExpireJobConfig.DATE_TIME_FORMATTER);
+    }
+    JobParametersBuilder jobParametersBuilder = new JobParametersBuilder();
+    jobParametersBuilder.addDate("now", new Date());
+    jobParametersBuilder.addString("expireTime", expireTime);
+    new Thread(() -> {
+      try {
+        jobLauncher.run(pointExpireJob, jobParametersBuilder.toJobParameters());
+      } catch (Exception e) {
+        SlackNotifier.notify(e);
+        FileBeatLogger.error(e);
+      }
+    }).start();
+  }
+}
--- src/main/java/com/kurly/cloud/point/api/point/batch/publish/PointOrderPublishManagementEndpoint.java
+++ src/main/java/com/kurly/cloud/point/api/point/batch/publish/PointOrderPublishManagementEndpoint.java
@@ -1,53 +1,35 @@
-package com.kurly.cloud.point.api.point.batch.controller;
+package com.kurly.cloud.point.api.point.batch.publish;
 
 import com.kurly.cloud.api.common.util.SlackNotifier;
 import com.kurly.cloud.api.common.util.logging.FileBeatLogger;
-import com.kurly.cloud.point.api.point.batch.expire.config.PointExpireJobConfig;
 import com.kurly.cloud.point.api.point.batch.publish.config.PointOrderPublishJobConfig;
 import java.time.LocalDateTime;
 import java.util.Date;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@Component
 @ConditionalOnProperty("batch.enable")
 @RequiredArgsConstructor
-@RestController
-public class PointBatchController {
+@Endpoint(id = "pointOrderPublish")
+public class PointOrderPublishManagementEndpoint {
 
   private final JobLauncher jobLauncher;
-  @Qualifier("pointExpireJob")
-  private final Job pointExpireJob;
   @Qualifier("pointOrderPublishJob")
   private final Job pointOrderPublishJob;
 
-  @GetMapping({"/batch/execute/expire/{expireTime}", "/batch/execute/expire/"})
-  void executeExpireBatch(@PathVariable(required = false) String expireTime) {
-    if (StringUtils.isEmpty(expireTime)) {
-      expireTime = LocalDateTime.now().format(PointExpireJobConfig.DATE_TIME_FORMATTER);
-    }
-    JobParametersBuilder jobParametersBuilder = new JobParametersBuilder();
-    jobParametersBuilder.addDate("now", new Date());
-    jobParametersBuilder.addString("expireTime", expireTime);
-    new Thread(() -> {
-      try {
-        jobLauncher.run(pointExpireJob, jobParametersBuilder.toJobParameters());
-      } catch (Exception e) {
-        SlackNotifier.notify(e);
-        FileBeatLogger.error(e);
-      }
-    }).start();
-  }
-
-  @GetMapping({"/batch/execute/order-publish/{publishDate}", "/batch/execute/order-publish/"})
-  void executeOrderPublishBatch(@PathVariable(required = false) String publishDate) {
+  @WriteOperation
+  void executeOrderPublishBatch(String publishDate) {
     if (StringUtils.isEmpty(publishDate)) {
       publishDate = LocalDateTime.now().format(PointOrderPublishJobConfig.DATE_TIME_FORMATTER);
     }
