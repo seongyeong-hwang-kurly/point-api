c805f722fcb3620438bf2f0d38b613fc880e860c
minseob.kim <minseob.kim@kurlycorp.com>
2020-07-28 13:14:30 +0900

    적립금 만료 배치가 정상동작하지 않는 문제를 수정하라 (#77)

--- src/main/java/com/kurly/cloud/point/api/point/service/ExpirePointAdapter.java
+++ src/main/java/com/kurly/cloud/point/api/point/service/ExpirePointAdapter.java
@@ -13,6 +13,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -24,6 +25,7 @@ class ExpirePointAdapter implements ExpirePointPort {
   private final PointService pointService;
   private final PointHistoryService pointHistoryService;
 
+  @Transactional
   @Override public PointExpireResult expireMemberPoint(long memberNumber,
                                                        LocalDateTime expireTime) {
     List<Point> expiredMemberPoint = pointService.getExpiredMemberPoint(memberNumber, expireTime);
