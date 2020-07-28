595700f7342b0f6d13c21f2750cfc73577cadf77
minseob.kim <minseob.kim@kurlycorp.com>
2020-07-28 17:02:06 +0900

    포인트 만료 배치 성능을 개선하라

--- src/main/java/com/kurly/cloud/point/api/point/batch/expire/PointExpireItemWriter.java
+++ src/main/java/com/kurly/cloud/point/api/point/batch/expire/PointExpireItemWriter.java
@@ -5,39 +5,42 @@ import com.kurly.cloud.point.api.point.domain.PointExpireResult;
 import com.kurly.cloud.point.api.point.port.in.ExpirePointPort;
 import java.time.LocalDateTime;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
+import java.util.concurrent.ForkJoinPool;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.annotation.BeforeStep;
-import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.stereotype.Component;
 
-@RequiredArgsConstructor
-@Component
-@StepScope
 public class PointExpireItemWriter implements ItemWriter<Long> {
 
   private final ExpirePointPort expirePointPort;
+  private final ForkJoinPool forkJoinPool;
   private StepExecution stepExecution;
   private String expireTime;
 
+  public PointExpireItemWriter(ExpirePointPort expirePointPort, int poolSize) {
+    this.expirePointPort = expirePointPort;
+    this.forkJoinPool = new ForkJoinPool(poolSize);
+  }
+
   @Override public void write(List<? extends Long> items) throws Exception {
-    items.parallelStream().forEach(memberNumber -> {
-      PointExpireResult result = expirePointPort.expireMemberPoint(memberNumber,
-          LocalDateTime.parse(expireTime, PointExpireJobConfig.DATE_TIME_FORMATTER));
+    forkJoinPool.submit(() -> {
+      items.parallelStream().forEach(memberNumber -> {
+        PointExpireResult result = expirePointPort.expireMemberPoint(memberNumber,
+            LocalDateTime.parse(expireTime, PointExpireJobConfig.DATE_TIME_FORMATTER));
 
-      ExecutionContext executionContext = stepExecution.getJobExecution().getExecutionContext();
-      long totalMemberCount = executionContext.getLong("totalMemberCount", 0);
-      long totalExpiredPointAmount = executionContext.getLong("totalExpiredPointAmount", 0);
-      long totalExpiredPointCount = executionContext.getLong("totalExpiredPointCount", 0);
+        ExecutionContext executionContext = stepExecution.getJobExecution().getExecutionContext();
+        long totalMemberCount = executionContext.getLong("totalMemberCount", 0);
+        long totalExpiredPointAmount = executionContext.getLong("totalExpiredPointAmount", 0);
+        long totalExpiredPointCount = executionContext.getLong("totalExpiredPointCount", 0);
 
-      executionContext.putLong("totalMemberCount", totalMemberCount + 1);
-      executionContext.putLong("totalExpiredPointCount",
-          totalExpiredPointCount + result.getExpiredPointSeq().size());
-      executionContext.putLong("totalExpiredPointAmount",
-          totalExpiredPointAmount + result.getTotalExpired());
-    });
+        executionContext.putLong("totalMemberCount", totalMemberCount + 1);
+        executionContext.putLong("totalExpiredPointCount",
+            totalExpiredPointCount + result.getExpiredPointSeq().size());
+        executionContext.putLong("totalExpiredPointAmount",
+            totalExpiredPointAmount + result.getTotalExpired());
+      });
+    }).get();
   }
 
   @BeforeStep
--- src/main/java/com/kurly/cloud/point/api/point/batch/expire/config/PointExpireJobConfig.java
+++ src/main/java/com/kurly/cloud/point/api/point/batch/expire/config/PointExpireJobConfig.java
@@ -3,6 +3,7 @@ package com.kurly.cloud.point.api.point.batch.expire.config;
 import com.kurly.cloud.point.api.point.batch.expire.PointExpireItemReader;
 import com.kurly.cloud.point.api.point.batch.expire.PointExpireItemWriter;
 import com.kurly.cloud.point.api.point.batch.expire.PointExpireJobListener;
+import com.kurly.cloud.point.api.point.port.in.ExpirePointPort;
 import com.kurly.cloud.point.api.point.repository.PointRepository;
 import com.kurly.cloud.point.api.point.util.SlackBot;
 import java.time.format.DateTimeFormatter;
@@ -12,6 +13,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.AbstractPagingItemReader;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -22,15 +24,21 @@ import org.springframework.context.annotation.Configuration;
 public class PointExpireJobConfig {
 
   public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ISO_DATE_TIME;
-  public static int CHUNK_SIZE = 1000;
+  private int chunkSize = 10_000;
+  private int poolSize = 10;
   private final StepBuilderFactory stepBuilderFactory;
-  private final PointExpireItemWriter pointExpireItemWriter;
+  private final ExpirePointPort expirePointPort;
   private final PointRepository pointRepository;
   private final SlackBot slackBot;
 
-  @Value("${batch.expire.chunkSize:1000}")
+  @Value("${batch.expire.chunkSize:10000}")
   public void setChunkSize(int chunkSize) {
-    CHUNK_SIZE = chunkSize;
+    this.chunkSize = chunkSize;
+  }
+
+  @Value("${batch.expire.poolSize:10}")
+  public void setPoolSize(int poolSize) {
+    this.poolSize = poolSize;
   }
 
   @Bean
@@ -43,17 +51,21 @@ public class PointExpireJobConfig {
 
   Step pointExpireJobStep(StepBuilderFactory stepBuilderFactory) {
     return stepBuilderFactory.get("pointExpireJobStep")
-        .<Long, Long>chunk(CHUNK_SIZE)
+        .<Long, Long>chunk(chunkSize)
         .reader(expirePointMemberNumberReader(null))
-        .writer(pointExpireItemWriter)
+        .writer(pointExpireItemWriter())
         .build();
+  }
+
+  public ItemWriter<Long> pointExpireItemWriter() {
+    return new PointExpireItemWriter(expirePointPort, poolSize);
   }
 
   @JobScope
   @Bean
   public AbstractPagingItemReader<Long> expirePointMemberNumberReader(
       @Value("#{jobParameters[expireTime]}") String expireTime) {
-    return new PointExpireItemReader(pointRepository, CHUNK_SIZE, expireTime);
+    return new PointExpireItemReader(pointRepository, chunkSize, expireTime);
   }
 }
 
