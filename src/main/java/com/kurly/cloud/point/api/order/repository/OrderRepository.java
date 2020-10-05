9506387f88eee52fe3debc7eab81c7a9c69caf35
minseob.kim <minseob.kim@kurlycorp.com>
2020-10-05 14:31:30 +0900

    첫 주문이 여러개 인 경우 적립금이 지급되지 않는 문제를 수정하라

--- src/main/java/com/kurly/cloud/point/api/order/repository/OrderRepository.java
+++ src/main/java/com/kurly/cloud/point/api/order/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package com.kurly.cloud.point.api.order.repository;
 
 import com.kurly.cloud.point.api.order.entity.Order;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,17 +12,19 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-  @Query("SELECT COUNT(o) FROM Order o" +
-      " WHERE o.memberNumber = :memberNumber " +
-      " AND o.orderStatus = 4" +
-      " AND o.orderProcessCode IN (0, 21, 22)")
-  int countMemberDeliveredOrder(@Param("memberNumber") long memberNumber);
+  @Query("SELECT COUNT(o) FROM Order o"
+      + " WHERE o.memberNumber = :memberNumber "
+      + " AND o.orderStatus = 4"
+      + " AND o.orderProcessCode IN (0, 21, 22)"
+      + " AND o.deliveredDateTime < :deliveredDateTime")
+  int countMemberDeliveredOrder(@Param("memberNumber") long memberNumber,
+                                @Param("deliveredDateTime") LocalDateTime deliveredDateTime);
 
-  @Query("SELECT o FROM Order o" +
-      " WHERE o.memberNumber = :memberNumber " +
-      " AND o.orderStatus IN (1, 4) " +
-      " AND o.orderProcessCode IN (0, 21, 22) " +
-      " ORDER BY o.orderNumber DESC")
+  @Query("SELECT o FROM Order o"
+      + " WHERE o.memberNumber = :memberNumber "
+      + " AND o.orderStatus IN (1, 4) "
+      + " AND o.orderProcessCode IN (0, 21, 22) "
+      + " ORDER BY o.orderNumber DESC")
   List<Order> findMemberOrderedOrder(@Param("memberNumber") long memberNumber,
                                      Pageable pageable);
 
