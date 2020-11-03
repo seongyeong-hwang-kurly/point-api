cef1c37ab001d9650df5a1ff57d1c7239b73f8e7
kyuwon.lee <kyuwon.lee@kurlycorp.com>
2020-11-03 21:44:50 +0900

    preconfig를 수정하라
    * 신규 CI에서 동작 될수 있도록 preconfig.sh를 수정하였습니다.

--- preconfig.sh
+++ preconfig.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-docker run --rm -u 1000:108 -v /var/log:/var/log -v "$PWD":/home/gradle/project -w /home/gradle/project \
-  -e SPRING_PROFILES_ACTIVE='dev,disable-discovery' -e TZ=Asia/Seoul -e JAVA_OPTS='-Xmx8g' gradle:jdk11 /home/gradle/project/gradlew clean build --info
+
+export SPRING_PROFILES_ACTIVE='dev,disable-discovery'
+./gradlew clean build --build-cache --info
+
