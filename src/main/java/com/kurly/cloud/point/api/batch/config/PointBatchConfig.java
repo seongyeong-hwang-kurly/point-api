0963298b50a8b9cb2b1d43b3efb0e7aeb59bdd8d
minseob.kim <minseob.kim@kurlycorp.com>
2020-11-24 11:05:35 +0900

    jar 실행에서 TransationManager를 잘 못 사용하는 문제를 수정하라

--- src/main/java/com/kurly/cloud/point/api/batch/config/PointBatchConfig.java
+++ src/main/java/com/kurly/cloud/point/api/batch/config/PointBatchConfig.java
@@ -1,6 +1,7 @@
 package com.kurly.cloud.point.api.batch.config;
 
 import java.time.format.DateTimeFormatter;
+import javax.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
@@ -8,10 +9,12 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.repository.support.MapJobRepositoryFactoryBean;
 import org.springframework.boot.task.TaskSchedulerBuilder;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableBatchProcessing
@@ -21,6 +24,7 @@ public class PointBatchConfig extends DefaultBatchConfigurer implements Scheduli
 
   public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ISO_DATE_TIME;
   private static final int SCHEDULER_POOL_SIZE = 5;
+  private final EntityManagerFactory emf;
 
   @Override protected JobRepository createJobRepository() throws Exception {
     MapJobRepositoryFactoryBean factory = new MapJobRepositoryFactoryBean();
@@ -33,6 +37,10 @@ public class PointBatchConfig extends DefaultBatchConfigurer implements Scheduli
         .build();
     taskScheduler.initialize();
     taskRegistrar.setTaskScheduler(taskScheduler);
+  }
+
+  @Override public PlatformTransactionManager getTransactionManager() {
+    return new JpaTransactionManager(emf);
   }
 }
 
