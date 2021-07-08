5451a9f44046eafd28eda23a249caa1004fb146b
minseob.kim <minseob.kim@kurlycorp.com>
2021-07-08 10:12:13 +0900

    발생예정적립금을 계산 할 때 적용된 적립 타입을 리턴하도록 변경하라

--- src/main/java/com/kurly/cloud/point/api/point/util/PointReserveCalculator.java
+++ src/main/java/com/kurly/cloud/point/api/point/util/PointReserveCalculator.java
@@ -6,6 +6,18 @@ import java.math.RoundingMode;
 
 public class PointReserveCalculator {
 
+  public static class Result {
+    ProductReserveType reserveType;
+    float reserveValue;
+    int reserve;
+
+    public Result(ProductReserveType reserveType, float reserveValue, int reserve) {
+      this.reserveType = reserveType;
+      this.reserveValue = reserveValue;
+      this.reserve = reserve;
+    }
+  }
+
   /**
    * 상품의 적립 타입과 회원의 적립률을 이용하여 발생 적립금을 계산한다.
    *
@@ -13,42 +25,51 @@ public class PointReserveCalculator {
    * @param productReserveValue     상품 적립금 적립 값 {@link ProductReserveType}
    * @param payPrice                결제 금액
    * @param memberPointReserveRatio 회원 적립률
-   * @return 발생 적립금
+   * @return 발생 예정 적립금과 정책 정보
    * @see ProductReserveType
    */
-  public static int calculate(ProductReserveType reserveType,
-                              int productReserveValue,
-                              int payPrice,
-                              float memberPointReserveRatio) {
+  public static Result calculate(ProductReserveType reserveType,
+                                 int productReserveValue,
+                                 int payPrice,
+                                 float memberPointReserveRatio) {
 
     int calculated;
+    ProductReserveType appliedReserveType = reserveType;
+    float reserveValue = productReserveValue;
 
     switch (reserveType) {
       case MEMBER:
         calculated = calculate(payPrice, memberPointReserveRatio);
+        reserveValue = memberPointReserveRatio;
         break;
       case FIXED:
         calculated = productReserveValue;
         break;
       case EXCLUDE:
-        return 0;
+        return new Result(appliedReserveType, 0, 0);
       case PERCENT:
         calculated = calculate(payPrice, productReserveValue);
         break;
       case MAX_PERCENT:
+        if (memberPointReserveRatio <= productReserveValue) {
+          appliedReserveType = ProductReserveType.MEMBER;
+          reserveValue = memberPointReserveRatio;
+        }
         calculated = calculate(payPrice, Math.min(memberPointReserveRatio, productReserveValue));
         break;
       case MAX_FIXED:
-        calculated = Math.min(
-            calculate(payPrice, memberPointReserveRatio),
-            productReserveValue
-        );
+        int memberPointReserve = calculate(payPrice, memberPointReserveRatio);
+        if (memberPointReserve <= productReserveValue) {
+          appliedReserveType = ProductReserveType.MEMBER;
+          reserveValue = memberPointReserveRatio;
+        }
+        calculated = Math.min(memberPointReserve, productReserveValue);
         break;
       default:
         throw new IllegalStateException("Unexpected value: " + reserveType);
     }
 
-    return Math.max(calculated, 0);
+    return new Result(appliedReserveType, reserveValue, Math.max(calculated, 0));
   }
 
   /**
