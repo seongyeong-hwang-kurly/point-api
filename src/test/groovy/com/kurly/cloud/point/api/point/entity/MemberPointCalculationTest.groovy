91a131dfad964674546a57ff226b423843dd19ea
Hyuksu Pyun <hyuksu.pyun@kurlycorp.com>
2021-10-27 18:44:46 +0900

    [WJZH-131] added spock and a test case for add function.

---
+++ src/test/groovy/com/kurly/cloud/point/api/point/entity/MemberPointCalculationTest.groovy
@@ -0,0 +1,16 @@
+package com.kurly.cloud.point.api.point.entity
+
+import spock.lang.Specification
+
+import java.time.LocalDateTime
+
+class MemberPointCalculationTest extends Specification {
+    def 'should return summed point'() {
+        given:
+        def point = new MemberPoint(1, 1000, 500, 500, LocalDateTime.now())
+        when:
+        point.plusPoint(500, 0)
+        then:
+        point.getTotalPoint() == 1500
+    }
+}
