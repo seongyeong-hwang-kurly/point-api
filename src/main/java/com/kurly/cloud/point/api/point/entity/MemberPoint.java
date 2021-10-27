36a01538c87541edbfa663491d9df4d0339a7559
Hyuksu Pyun <hyuksu.pyun@kurlycorp.com>
2021-10-27 18:49:58 +0900

    [WJZH-131] removed unnecessary code("@Transient").

--- src/main/java/com/kurly/cloud/point/api/point/entity/MemberPoint.java
+++ src/main/java/com/kurly/cloud/point/api/point/entity/MemberPoint.java
@@ -1,21 +1,13 @@
 package com.kurly.cloud.point.api.point.entity;
 
 import com.kurly.cloud.point.api.point.entity.converter.UnixTimestampConverter;
-import java.time.LocalDateTime;
-import javax.persistence.Column;
-import javax.persistence.Convert;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Transient;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.OptimisticLockType;
 import org.hibernate.annotations.OptimisticLocking;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @DynamicUpdate
 @OptimisticLocking(type = OptimisticLockType.DIRTY)
@@ -44,10 +36,6 @@ public class MemberPoint {
   @Column(name = "update_time")
   LocalDateTime updateTime;
 
-  /**
-   * 적립금을 더함.
-   */
-  @Transient
   public void plusPoint(long freePoint, long cashPoint) {
     setTotalPoint(getTotalPoint() + freePoint + cashPoint);
     setFreePoint(getFreePoint() + freePoint);
@@ -55,10 +43,6 @@ public class MemberPoint {
     setUpdateTime(LocalDateTime.now());
   }
 
-  /**
-   * 적립금을 뺌.
-   */
-  @Transient
   public void minusPoint(long freePoint, long cashPoint) {
     setTotalPoint(getTotalPoint() - freePoint - cashPoint);
     setFreePoint(getFreePoint() - freePoint);
@@ -66,10 +50,6 @@ public class MemberPoint {
     setUpdateTime(LocalDateTime.now());
   }
 
-  /**
-   * 상환할 빚이 있는지 계산 한다.
-   */
-  @Transient
   public long getRepayAmount(long publishedAmount) {
     if (getTotalPoint() >= publishedAmount) {
       return 0;
@@ -85,10 +65,6 @@ public class MemberPoint {
     return repayAmount;
   }
 
-  /**
-   * 적립금을 사용하기에 충분히 가지고 있는지 계산한다.
-   */
-  @Transient
   public boolean isEnough(Long point, boolean settle) {
     return settle ? getCashPoint() >= point : getTotalPoint() >= point;
   }
