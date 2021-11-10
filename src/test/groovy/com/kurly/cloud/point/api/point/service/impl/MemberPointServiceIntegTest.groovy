3504cb11a61badd1a627224fb24b25dc7f93f177
hyuksu.pyun <hyuksu.pyun@kurlycorp.com>
2021-11-10 16:57:13 +0900

    [WJZH-203] added refund test case.

--- src/test/groovy/com/kurly/cloud/point/api/point/service/impl/MemberPointServiceIntegTest.groovy
+++ src/test/groovy/com/kurly/cloud/point/api/point/service/impl/MemberPointServiceIntegTest.groovy
@@ -1,5 +1,7 @@
 package com.kurly.cloud.point.api.point.service.impl
 
+import com.kurly.cloud.point.api.point.domain.consume.CancelOrderConsumePointRequest
+import com.kurly.cloud.point.api.point.domain.consume.OrderConsumePointRequest
 import com.kurly.cloud.point.api.point.domain.history.MemberPointHistoryListRequest
 import com.kurly.cloud.point.api.point.domain.publish.PublishPointRequest
 import org.springframework.beans.factory.annotation.Autowired
@@ -17,6 +19,11 @@ import java.time.LocalDateTime
 @ActiveProfiles("local")
 class MemberPointServiceIntegTest extends Specification {
     public static final int MEMBER_NO = 999999999
+    public static final int ORDER_NUMBER = 1
+    public static final int FIRST_TRY = 3
+    public static final int SECOND_TRY = 2
+    public static final int FIRST_UNIT_PRICE = 100
+    public static final int SECOND_UNIT_PRICE = 200
     @Shared
     MemberPointHistoryListRequest historyRequest
     @Shared
@@ -28,6 +35,10 @@ class MemberPointServiceIntegTest extends Specification {
 
     @Subject
     @Autowired
+    ConsumePointServiceV2 consumePointService
+
+    @Subject
+    @Autowired
     MemberPointService memberPointService
 
     def setup() {
@@ -35,40 +46,57 @@ class MemberPointServiceIntegTest extends Specification {
         expiredAt = LocalDateTime.parse("2021-12-31T23:59:59.99")
     }
 
-    def '1. should give point to a member'() {
+    def '1. should give point to a member(/v1/publish)'() {
         given:
-        publishNTimes(3, 100)
-        publishNTimes(1, 300)
+        publishNTimes(FIRST_TRY, FIRST_UNIT_PRICE)
+        publishNTimes(SECOND_TRY, SECOND_UNIT_PRICE)
         when:
         def pointHistories = memberPointService.getMemberHistoryList(historyRequest)
         then:
-        pointHistories.size() == 4
+        pointHistories.size() == FIRST_TRY + SECOND_TRY
     }
 
     private publishNTimes(int times, int point) {
         (1..times).forEach {
             def pointRequest = PublishPointRequest.builder()
-                    .point(point).orderNumber(1)
+                    .point(point).orderNumber(ORDER_NUMBER)
                     .historyType(1)
                     .memberNumber(MEMBER_NO).expireDate(expiredAt).build()
             publishPointService.publish(pointRequest)
         }
     }
 
-    def '2. should return accumulated points summary'() {
+    def '2. should return available member point (/v1/available/{memberNumber})'() {
         when:
-        def summary = memberPointService.getMemberPointSummary(MEMBER_NO)
+        def memberPoint = memberPointService.getMemberPoint(MEMBER_NO)
         then:
-        summary.getAmount() == 600
-        summary.getNextExpireAmount() == 0
-        println(summary.getNextExpireDate())
+        memberPoint.getTotalPoint() == FIRST_TRY * FIRST_UNIT_PRICE + SECOND_TRY * SECOND_UNIT_PRICE
     }
 
-    def '3. should return available member point'() {
+    def '3. should be able to use the points'() {
+        given:
+        def orderRequest = OrderConsumePointRequest.builder()
+            .orderNumber(ORDER_NUMBER)
+            .memberNumber(MEMBER_NO)
+            .point(350)
+            .build()
         when:
-        def point = memberPointService.getMemberPoint(MEMBER_NO)
+        consumePointService.consumeByOrder(orderRequest)
         then:
-        point.getTotalPoint() == 600
+        350L == memberPointService.getMemberPoint(MEMBER_NO).getTotalPoint()
+    }
+
+    def '4. should refund point already used'() {
+        given:
+        def cancelRequest = CancelOrderConsumePointRequest.builder()
+                .memberNumber(MEMBER_NO)
+                .point(300)
+                .orderNumber(ORDER_NUMBER)
+                .build()
+        when:
+        consumePointService.cancelConsumeByOrder(cancelRequest)
+        then:
+        650L == memberPointService.getMemberPoint(MEMBER_NO).getTotalPoint()
     }
 
 }
